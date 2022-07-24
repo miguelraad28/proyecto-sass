57d9e12a94fb7f6aa1a971f4c4c0368076426ce6
miguelraad28 <miguelraad2020@gmail.com>
2022-07-24 18:26:17 -0300

    .

--- productos.js
+++ productos.js
@@ -25,21 +25,21 @@ if(localStorage.getItem("carrito")){
 }else{
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
-
+let resultadosDeBusqueda = []
 const productos = 
-[{id: 0, nombre: "oráculo conexión con el universo box", precio: 3990, stock: 20, img:"../images/productos/cartas/universobox.jpg", cantidad: 0},
-{id: 1, nombre: "oráculo conexión con el universo simple", precio: 2650, stock: 35, img:"../images/productos/cartas/universosolo.jpg", cantidad: 0},
-{id: 2, nombre: "lectura de cartas 45min", precio: 4800, stock: 100000, img:"../images/productos/tarot.jpg", cantidad: 0},
-{id: 3, nombre: "kit personal 7 chakras", precio: 2650, stock: 12, img:"../images/productos/limpieza/chakras.jpg", cantidad: 0},
-{id: 4, nombre: "spry limpieza energética sandalo y champa", precio: 650, stock: 16, img:"../images/productos/limpieza/sandaloychampa.jpg", cantidad: 0},
-{id: 5, nombre: "spry limpieza energética de descarga", precio: 650, stock: 11, img:"../images/productos/limpieza/limpieza.jpg", cantidad: 0},
-{id: 6, nombre: "spry limpieza energética ruda y romero", precio: 690, stock: 19, img:"../images/productos/limpieza/rudayromero.jpg", cantidad: 0},
-{id: 7, nombre: "spry limpieza energética abundancia y fortuna", precio: 780, stock: 25, img:"../images/productos/limpieza/abundanciayfortuna.jpg", cantidad: 0},
-{id: 8, nombre: "vela de soja de la abundancia y fortuna", precio: 890, stock: 2, img:"../images/productos/velas/abundancia.jpg", cantidad: 0},
-{id: 9, nombre: "vela de soja del amor", precio: 890, stock: 22, img:"../images/productos/velas/amor.jpg", cantidad: 0},
-{id: 10, nombre: `vela de soja "buda"`, precio: 950, stock: 20, img:"../images/productos/velas/buda.jpg", cantidad: 0},
-{id: 11, nombre: "vela de soja de limpieza energética", precio: 920, stock: 20, img:"../images/productos/velas/limpieza.jpg", cantidad: 0},
-{id: 12, nombre: "vela de soja de la seducción", precio: 990, stock: 9, img:"../images/productos/velas/seduccion.jpg", cantidad: 0},]
+[{id: 0, nombre: "oráculo conexión con el universo box", precio: 3990, stock: 20, img:"../images/productos/cartas/universobox.jpg"},
+{id: 1, nombre: "oráculo conexión con el universo simple", precio: 2650, stock: 35, img:"../images/productos/cartas/universosolo.jpg"},
+{id: 2, nombre: "lectura de cartas 45min", precio: 4800, stock: 100000, img:"../images/productos/tarot.jpg"},
+{id: 3, nombre: "kit personal 7 chakras", precio: 2650, stock: 12, img:"../images/productos/limpieza/chakras.jpg"},
+{id: 4, nombre: "spry limpieza energética sandalo y champa", precio: 650, stock: 16, img:"../images/productos/limpieza/sandaloychampa.jpg"},
+{id: 5, nombre: "spry limpieza energética de descarga", precio: 650, stock: 11, img:"../images/productos/limpieza/limpieza.jpg"},
+{id: 6, nombre: "spry limpieza energética ruda y romero", precio: 690, stock: 19, img:"../images/productos/limpieza/rudayromero.jpg"},
+{id: 7, nombre: "spry limpieza energética abundancia y fortuna", precio: 780, stock: 25, img:"../images/productos/limpieza/abundanciayfortuna.jpg"},
+{id: 8, nombre: "vela de soja de la abundancia y fortuna", precio: 890, stock: 2, img:"../images/productos/velas/abundancia.jpg"},
+{id: 9, nombre: "vela de soja del amor", precio: 890, stock: 22, img:"../images/productos/velas/amor.jpg"},
+{id: 10, nombre: `vela de soja "buda"`, precio: 950, stock: 20, img:"../images/productos/velas/buda.jpg"},
+{id: 11, nombre: "vela de soja de limpieza energética", precio: 920, stock: 20, img:"../images/productos/velas/limpieza.jpg"},
+{id: 12, nombre: "vela de soja de la seducción", precio: 990, stock: 9, img:"../images/productos/velas/seduccion.jpg"},]
 
 const divProductos = document.getElementById("idDivProductos")
 productos.forEach((producto, id) => {
@@ -66,22 +66,21 @@ productos.forEach((producto, id) => {
     </div>
     `
 })
-
 const inputBusqueda = document.getElementById("idInputBusqueda")
 const botonBusqueda = document.getElementById("idBotonBusqueda")
 botonBusqueda.addEventListener("change", (event) =>{
     event.preventDefault()
-    funcionBusqueda(inputBusqueda.value)
+    busquedaDeProductos(inputBusqueda.value)
 })
 botonBusqueda.addEventListener("click", (event) =>{
     event.preventDefault()
-    funcionBusqueda(inputBusqueda.value)
+    busquedaDeProductos(inputBusqueda.value)
 })
-function funcionBusqueda(inputBusqueda){
-    const resultadosDeBusqueda = productos.filter((producto) => producto.nombre.includes(inputBusqueda.toLowerCase()))
-    funcionMostrarHTML(resultadosDeBusqueda)
+function busquedaDeProductos(inputBusqueda){
+    resultadosDeBusqueda = productos.filter((producto) => producto.nombre.includes(inputBusqueda.toLowerCase()))
+    mostrarBusquedaDeProductos(resultadosDeBusqueda)
 }
-function funcionMostrarHTML(resultadosDeBusqueda){
+function mostrarBusquedaDeProductos(resultadosDeBusqueda){
     divProductos.innerHTML = ``
     for(let producto of resultadosDeBusqueda){
         divProductos.innerHTML += `
@@ -108,14 +107,24 @@ function funcionMostrarHTML(resultadosDeBusqueda){
         `
     }
 }
+let productoAñadido
 productos.forEach((producto) => {
     let botonAgregarACarrito = document.getElementById(`producto${producto.id}`).lastElementChild.lastElementChild
-    botonAgregarACarrito.addEventListener("click", () =>{
-        if(carrito.find(productoEnCarrito => productoEnCarrito.id == producto.id)){
-            producto.cantidad += 1
-            console.log(carrito)
+    botonAgregarACarrito.addEventListener("click", (productos) =>{
+        funcionAgregarACarrito(producto)
+        productoAñadido = {
+            id: producto.id,
+            nombre: producto.nombre,
+            precio: producto.precio,
+            img: producto.img,
+            cantidad: producto.cantidad = 0
+        }
+        if(carrito.find(productoEnCarrito => productoEnCarrito.id === productoAñadido.id)){
+            carrito.filter((productoEnCarrito) => productoEnCarrito.id === productoAñadido.id)
         }else{
-            producto.cantidad += 1
+            carrito.find(productoEnCarrito => productoEnCarrito.id === productoAñadido.id)
+            productoAñadido.cantidad += 1
+            carrito.push(productoAñadido);
             divCarritoDeCompras.innerHTML += `
             <div class="productoEnCarrito" id="${producto.id}">
                 <div class="imagenProductoEnCarrito">
@@ -135,9 +144,46 @@ productos.forEach((producto) => {
                 </div>
             </div>
             `
-            carrito.push(producto);
             console.log(carrito)
         }
         localStorage.setItem("carrito", JSON.stringify(carrito))
     })
 })
+function funcionAgregarACarrito(producto){
+        productoAñadido = {
+            id: producto.id,
+            nombre: producto.nombre,
+            precio: producto.precio,
+            img: producto.img,
+            cantidad: producto.cantidad = 0
+        }
+        if(carrito.find(productoEnCarrito => productoEnCarrito.id == productoAñadido.id)){
+            carrito.find(productoEnCarrito => productoEnCarrito.cantidad += 1)
+            console.log(carrito)
+        }else{
+            carrito.find(productoEnCarrito => productoEnCarrito.id === productoAñadido.id)
+            productoAñadido.cantidad += 1
+            carrito.push(productoAñadido);
+            divCarritoDeCompras.innerHTML += `
+            <div class="productoEnCarrito" id="${producto.id}">
+                <div class="imagenProductoEnCarrito">
+                    <img src="${producto.img}">
+                </div>
+                <div class="tituloPrecioCantidadProductoEnCarrito">
+                    <div class="tituloProductoEnCarrito">
+                        <h6>${producto.nombre}</h6>
+                    </div>
+                    <div class="precioCantidadProductoEnCarrito">
+                        <h6>$ ${producto.precio}</h6>
+                        <input type="number" value="${producto.cantidad}">
+                    </div>
+                </div>
+                <div class="eliminarProductoEnCarrito">
+                    <button>X</button>
+                </div>
+            </div>
+            `
+            console.log(carrito)
+        }
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+}
